cf06fc6c612ee00c52bbd68a80dea250033cd17c
WiktorStaniszewski <wiktorst.8@gmail.com>
2023-12-20 17:05:04 +0100

    it finally is in the middle

--- animations.js
+++ animations.js
@@ -2,11 +2,11 @@ function dissapear() {
     const x = document.getElementById("cover");
     const y = document.getElementById("the_right_page")
     if (x.style.display === "none") {
-      x.style.display = "block";
+      x.style.display = "flex";
       y.style.display = "none";
     } else {
       x.style.display = "none";
-      y.style.display = "block";
+      y.style.display = "flex";
     }
   }
 // on click disappears the cover page and appears the right page
