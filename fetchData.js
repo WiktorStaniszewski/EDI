7a6964fbb957379a111f97b4fa6921c122ad9ab9
Jakub Dziektarz <73693198+MrZakurzacz@users.noreply.github.com>
2023-12-20 02:52:53 +0100

    skrypt fetchData.js i table_style.css
    fetchData.js do obslugi pobierania danych z API, w skrypcie mozna zmienic key z ktorego pobiera.
    table_style.css jako temp formatowanie tabelki

---
+++ fetchData.js
@@ -0,0 +1,43 @@
+
+async function fetchAndDisplayData() {
+    const apiKey = '84223600'; // Will change when proper schema will be used
+    const apiUrl = `https://my.api.mockaroo.com/users.json?key=${apiKey}`;
+  
+    try { // If mockaroo servers fail for any reason
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+  
+      // Generate table and display data function
+      displayDataInTable(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  
+  // Function to display data in a table on the webpage
+  function displayDataInTable(data) {
+    const table = document.getElementById('data');
+    table.innerHTML = '';
+  
+    if (data && data.length > 0) {
+      const headers = Object.keys(data[0]);
+  
+      // Create table headers
+      const headerRow = table.createTHead().insertRow();
+      headers.forEach(header => {
+        const th = document.createElement('th');
+        th.textContent = header;
+        headerRow.appendChild(th);
+      });
+  
+      // Populate table rows with data
+      data.forEach(item => {
+        const row = table.insertRow();
+        headers.forEach(header => {
+          const cell = row.insertCell();
+          cell.textContent = item[header];
+        });
+      });
+    }
+  }
+  
