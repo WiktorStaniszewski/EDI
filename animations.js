da59eae1b6d91eee991f7a9383d84b994f46f37f
WiktorStaniszewski <wiktorst.8@gmail.com>
2023-12-26 10:14:29 +0100

    add the ^button, remove gototop

--- animations.js
+++ animations.js
@@ -1,12 +1,15 @@
 function dissapear() {
-    const x = document.getElementById("cover");
-    const y = document.getElementById("the_right_page")
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-      y.style.display = "none";
-    } else {
-      x.style.display = "none";
-      y.style.display = "flex";
-    }
+  const x = document.getElementById("cover");
+  const y = document.getElementById("the_right_page")
+  const z = document.getElementById("button2")
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+    y.style.display = "none";
+    z.style.display = "none";
+  } else {
+    x.style.display = "none";
+    y.style.display = "flex";
+    z.style.display = "block";
   }
+}
 // on click disappears the cover page and appears the right page
