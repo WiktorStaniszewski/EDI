a0e1c4fe0c51d074b08927411af497b5bc361d5c
WiktorStaniszewski <wiktorst.8@gmail.com>
2023-12-28 20:08:58 +0100

    generate data button&links

--- fetchData.js
+++ fetchData.js
@@ -2,16 +2,13 @@ async function fetchAndDisplayData() {
     const apiKey = '84223600'; // Will change when proper schema will be used
     const apiUrl = `https://my.api.mockaroo.com/users.json?key=${apiKey}`;
     const y = document.getElementById("scrollable");
-    const x = document.getElementById("cover");
-    if (y.style.display == "none") {  // This makes onclick- display and hide the section
-      y.style.display = "block";
-    } else {
-      y.style.display = "none";
-    }
     try { // If mockaroo servers fail for any reason
       const response = await fetch(apiUrl);
       const data = await response.json();
-  
+      if (y.style.display == "none") { // if clicked first time it will be shown
+        y.style.display = "block";
+      }
+      
       // Generate table and display data function
       displayDataInTable(data);
 
@@ -19,6 +16,15 @@ async function fetchAndDisplayData() {
       createGraphs(data);
     } catch (error) {
       console.error('Error fetching data:', error);
+    }
+  }
+  function hideandshow() {
+    const y = document.getElementById("scrollable");
+    const x = document.getElementById("cover");
+    if (y.style.display == "none") {  // This makes onclick- display and hide the section
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
     }
   }
   
